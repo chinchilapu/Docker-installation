869303d77b62227c523b3b16c230ecd2bedc9453
Adharsh Chinchilapu <chinchilapu@lpu.in>
2025-07-09 19:31:41 +0530

    Create bash1.sh

---
+++ bash1.sh
@@ -0,0 +1,18 @@
+mkdir hello-world
+cd hello-world
+nano file.py
+"
+print("Hello World")
+"
+nano dockerfile
+"
+FROM python:3.9-slim
+WORKDIR /file
+COPY . /file
+CMD ["python", "file.py"]
+"
+docker build -t username/hello-world:latest .
+docker run username/hello-world:latest
+docker login
+docker tag username/hello-world:latest username/hello-world:latest
+docker push username/hello-world:latest
