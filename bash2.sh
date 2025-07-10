1862ddf51284d47bfae9fb54a2ae981fab705da9
Adharsh Chinchilapu <chinchilapu@lpu.in>
2025-07-10 21:11:25 +0530

    Update bash2.sh

--- bash2.sh
+++ bash2.sh
@@ -1,18 +1,20 @@
 mkdir doc-noc
 cd doc-noc
 nano file.py
-"
+/*
 from flask import Flask
 file = Flask(__name__)
 file.route('/')
 def hello():
   return "Hey there! what's up"
 file.run(host = "0.0.0.0", port = 5000)
-"
+*/
+
 nano requirments.txt 
-"
+/*
 flask
-"
+*/
+
 nano Dockerfile
 /*
 FROM python:3.9-slim
@@ -22,3 +24,18 @@ RUN pip install --no-cache-dir -r requirements.txt
 CMD ["python", "file.py"]
 */
 
+nano docker-compose.yml
+/*
+version: "3.8"
+
+services:
+  web:
+    build:
+      context: .
+      dockerfile: Dockerfile   # or "dockerfile" if yours is lowercase
+    ports:
+      - "5000:5000"
+    restart: always
+*/
+docker-compose up --build
+docker 
