85a7a2328a32d0dbc38649da4faa45aea3b38161
Adharsh Chinchilapu <chinchilapu@lpu.in>
2025-07-10 21:05:30 +0530

    Update bash2.sh

--- bash2.sh
+++ bash2.sh
@@ -1,1 +1,24 @@
+mkdir doc-noc
+cd doc-noc
+nano file.py
+"
+from flask import Flask
+file = Flask(__name__)
+file.route('/')
+def hello():
+  return "Hey there! what's up"
+file.run(host = "0.0.0.0", port = 5000)
+"
+nano requirments.txt 
+"
+flask
+"
+nano Dockerfile
+/*
+FROM python:3.9-slim
+WORKDIR /app
+COPY . /app
+RUN pip install --no-cache-dir -r requirements.txt
+CMD ["python", "file.py"]
+*/
 
